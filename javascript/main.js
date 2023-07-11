bd7cb1be29636fa5b5869697fbfc4bf39b25a12c
Tafadzwa <musinachirevotafadzwa@gmail.com>
2023-07-11 14:49:48 +0200

    added HTML boiler Template

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+
+
+class Students{
+    constructor(name,surname,student_id){
+        this.name =name;
+        this.surname = surname;
+        this .student_id = student_id;
+    }
+}
+
+const newStudent = new Students("Tafadzwa","Musinachirevo",34);
+console.log(newStudent);
